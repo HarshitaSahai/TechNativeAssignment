3b185dccae97d1a7943d1525aaa14af8efa9a885
Harshita Sahai <40337803+HarshitaSahai@users.noreply.github.com>
2020-08-17 23:19:28 +0530

    Create project1.js

---
+++ JumpStart_Assignment/project1.js
@@ -0,0 +1,8 @@
+// Declaring variables
+
+var task1, task2, task3;
+task1 = "Pay phone bill";
+task2 = "Write best-selling novel";
+task3 = "Walk the dog"; 
+
+console.log(task1 + " " +  task2 + " "+ task3)
